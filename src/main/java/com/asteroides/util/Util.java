b637bd874bd75253b09a9939146ecafa98868c1f
carolinedesousasantos <caroline.dsosa@gmail.com>
2022-06-16 13:17:13 -0300

    Date formatter.

---
+++ src/main/java/com/asteroides/util/Util.java
@@ -0,0 +1,12 @@
+package com.asteroides.util;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Util {
+    public static String formatCurrentDate(LocalDateTime now) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String dateFormat = dtf.format(now);
+        return dateFormat;
+    }
+}
