37d88c443c2c77296ae828d7d44806c4f9e955ee
carolinedesousasantos <caroline.dsosa@gmail.com>
2022-06-16 19:19:11 -0300

    Using Nasa development api key.

--- src/main/java/com/asteroides/service/NasaService.java
+++ src/main/java/com/asteroides/service/NasaService.java
@@ -1,5 +1,6 @@
 package com.asteroides.service;
 
+import com.asteroides.exceptions.ValueNotAcceptedException;
 import com.asteroides.modelo.Asteroides;
 import com.asteroides.modelo.AsteroidsInformation;
 import com.asteroides.util.Util;
@@ -13,15 +14,19 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class NasaService {
 
-  public String getUrl(Long day) {
+  public String getUrl(Long day) throws ValueNotAcceptedException {
     String star_date = Util.formatCurrentDate(LocalDateTime.now());
     String end_date = Util.formatCurrentDate(LocalDateTime.now().plusDays(day));
-    return
-        "https://api.nasa.gov/neo/rest/v1/feed?start_date="
-            + star_date
-            + "&end_date="
-            + end_date
-            + "&api_key=DEMO_KEY";
+
+    if (day > 0 && day <= 7) {
+      return  "https://api.nasa.gov/neo/rest/v1/feed?start_date="
+              + star_date
+              + "&end_date="
+              + end_date
+              + "&api_key=zdUP8ElJv1cehFM0rsZVSQN7uBVxlDnu4diHlLSb";
+    } else {
+      throw new ValueNotAcceptedException();
+    }
   }
 
   public List<Asteroides> getNasaList(String url) {
@@ -32,10 +37,10 @@ public class NasaService {
 
   public List<AsteroidsInformation> getPotentiallyHazardousAsteroid(List<Asteroides> list) {
     return list.stream()
-            .map(objects -> objects.getNear_earth_objects().values())
-            .flatMap(l -> l.stream())
-            .flatMap(List::stream)
-            .filter(AsteroidsInformation::isPotentiallyHazardousAsteroid)
-            .collect(Collectors.toList());
+        .map(objects -> objects.getNear_earth_objects().values())
+        .flatMap(l -> l.stream())
+        .flatMap(List::stream)
+        .filter(AsteroidsInformation::isPotentiallyHazardousAsteroid)
+        .collect(Collectors.toList());
   }
 }
