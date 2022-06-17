b97bea8fd0faa1e1f4dadedda7d6db566808b7c1
carolinedesousasantos <caroline.dsosa@gmail.com>
2022-06-17 08:56:30 -0300

    Improvements on ValueNotAcceptedException.

--- src/main/java/com/asteroides/exceptions/ValueNotAcceptedException.java
+++ src/main/java/com/asteroides/exceptions/ValueNotAcceptedException.java
@@ -5,5 +5,4 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.NOT_ACCEPTABLE, reason = "Value needs to be between 1 and 7.")
 public class ValueNotAcceptedException extends Exception {
-  public ValueNotAcceptedException() {}
 }
